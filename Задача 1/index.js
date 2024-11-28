c18941133701a7012e51b9079d032f8e10a3d327
TimSoarer <timomavr@gmail.com>
2024-11-28 18:50:58 +0500

    Added files needed for 2nd practice

---
+++ Задача 1/index.js
@@ -0,0 +1,10 @@
+// Исправьте код этой программы так, что в консоль выводился результат выполнения программы, а не ошибка
+const currentYear = 2024
+
+student = Илья
+let year = "2006"
+let age = "currentYear" - year
+
+console.log("Студент:", student);
+console.log("Год рождения:", year);
+console.log("Возраст:", age);
---
+++ Задача 3/index.js
@@ -0,0 +1,28 @@
+// Сделайте код программы короче, используя инкремент.
+
+let index = 0; // Счетчик
+
+// Бегун 1
+let runner1 = "Денис";
+index = index + 1;
+console.log(index, runner1);
+
+// Бегун 2
+let runner2 = "Виктория";
+index = index + 1;
+console.log(index, runner2);
+
+// Бегун 3
+let runner3 = "Антон";
+index = index + 1;
+console.log(index, runner3);
+
+// Бегун 4
+let runner4 = "Лена";
+index = index + 1;
+console.log(index, runner4);
+
+// Бегун 5
+let runner5 = "Никита";
+index = index + 1;
+console.log(index, runner5);
